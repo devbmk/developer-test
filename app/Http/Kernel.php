379ea3fa57f9072626013c904c7acacb3d30a1e2
devbmk <lyvanna1987@outlook.com>
2024-06-08 09:54:12 -0400

    add active user middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,7 @@ class Kernel extends HttpKernel
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'active' => \App\Http\Middleware\ActivateUser::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
